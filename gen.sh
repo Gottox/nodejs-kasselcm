6c27ec5e966b1fde8252c38c761b6c675c42669d
Enno Boland <g@s01.de>
2016-08-04 11:19:45 +0200

    initial checkin

---
+++ gen.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+upstream=$(git remote get-url Gottox | sed 's#:#/#; s#.*git@#https://#;')
+
+init() {
+	sed "s#^talk-\(.*\)#\1#" | \
+		while read talk; do
+			[ -d "$talk" ] && git rm -f "$talk" > /dev/null
+			git submodule add --reference . --force -b "talk-$talk" -- "$upstream" "$talk" >&2
+			echo "<li><a href='$talk'>$talk</a></li>"
+		done
+	git submodule update >&2
+}
+
+exec > index.html
+cat <<EOF
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Talks</title>
+		 <link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<h1>Talks</h1>
+		<ul>
+EOF
+git branch --list 'talk-*' | sed 's/^[* ] //' | init
+cat <<EOF
+		</ul>
+	</body>
+</html>
+EOF
